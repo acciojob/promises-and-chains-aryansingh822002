daf74f0f75a6e68f586c08c43e8b2614454d9812
Shubham Singh <128978409+aryansingh822002@users.noreply.github.com>
2025-08-20 17:58:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById("voteForm").addEventListener("submit", function (e) {
 
   // Validation
   if (!name || isNaN(age)) {
-    alert("Please enter valid details.");
+    alert("Please enter valid details");
     return;
   }
 
